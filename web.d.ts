ca78b74d95505fcd1c6f56268f1edbf36d135336
nin-jin <nin-jin@users.noreply.github.com>
2022-07-10 07:07:22 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1471,6 +1471,7 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_link extends $mol_view {
+        uri(): string;
         dom_name(): string;
         attr(): {
             href: string;
@@ -1484,7 +1485,7 @@ declare namespace $ {
         event(): {
             click: (event?: any) => any;
         };
-        uri(): string;
+        uri_toggle(): string;
         hint(): string;
         target(): string;
         file_name(): string;
@@ -1499,11 +1500,14 @@ declare namespace $ {
 
 declare namespace $.$$ {
     class $mol_link extends $.$mol_link {
+        uri_toggle(): string;
         uri(): string;
+        uri_off(): string;
         uri_native(): URL;
         current(): boolean;
         file_name(): string;
         minimal_height(): number;
+        external(): boolean;
         target(): '_self' | '_blank' | '_top' | '_parent' | string;
     }
 }
