abeeb121f7b1e96729efc8ffee45290359a7ddb1
nin-jin <nin-jin@users.noreply.github.com>
2022-09-03 07:07:20 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -901,6 +901,7 @@ declare namespace $ {
         disabled(): boolean;
         tab_index(): number;
         hint(): string;
+        hint_safe(): string;
         error(): string;
     }
 }
@@ -1021,6 +1022,7 @@ declare namespace $.$$ {
         event_key_press(event: KeyboardEvent): void;
         tab_index(): number;
         error(): string;
+        hint_safe(): string;
         sub_visible(): ($mol_view_content | $mol_speck)[];
     }
 }
@@ -1554,6 +1556,7 @@ declare namespace $ {
         };
         uri_toggle(): string;
         hint(): string;
+        hint_safe(): string;
         target(): string;
         file_name(): string;
         current(): boolean;
@@ -1576,6 +1579,7 @@ declare namespace $.$$ {
         minimal_height(): number;
         external(): boolean;
         target(): '_self' | '_blank' | '_top' | '_parent' | string;
+        hint_safe(): string;
     }
 }
 
--- node.test.js
+++ node.test.js
@@ -2955,7 +2955,7 @@ var $;
                 disabled: this.disabled(),
                 role: "button",
                 tabindex: this.tab_index(),
-                title: this.hint()
+                title: this.hint_safe()
             };
         }
         sub() {
@@ -2991,6 +2991,9 @@ var $;
         }
         hint() {
             return "";
+        }
+        hint_safe() {
+            return this.hint();
         }
         error() {
             return "";
@@ -3175,6 +3178,15 @@ var $;
                     return $mol_fail_hidden(error);
                 }
                 return String(error.message ?? error);
+            }
+            hint_safe() {
+                try {
+                    return this.hint();
+                }
+                catch (error) {
+                    $mol_fail_log(error);
+                    return '';
+                }
             }
             sub_visible() {
                 return [
@@ -4765,7 +4777,7 @@ var $;
             return {
                 ...super.attr(),
                 href: this.uri_toggle(),
-                title: this.hint(),
+                title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
                 mol_link_current: this.current()
@@ -4790,6 +4802,9 @@ var $;
         }
         hint() {
             return "";
+        }
+        hint_safe() {
+            return this.hint();
         }
         target() {
             return "_self";
@@ -4917,6 +4932,15 @@ var $;
             }
             target() {
                 return this.external() ? '_blank' : '_self';
+            }
+            hint_safe() {
+                try {
+                    return this.hint();
+                }
+                catch (error) {
+                    $mol_fail_log(error);
+                    return '';
+                }
             }
         }
         __decorate([
@@ -7730,7 +7754,7 @@ var $;
             scheduled = false;
             await $mol_test_run();
             $$.$mol_test_complete();
-        }, 0);
+        }, 1000);
     }
     $_1.$mol_test_schedule = $mol_test_schedule;
     $_1.$mol_test_mocks.push(context => {
